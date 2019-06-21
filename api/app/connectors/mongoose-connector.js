4d133d8a2802d7ed884fab6f464f27eeb0f395a4
kostya vlasov <flockast@gmail.com>
2019-06-22 02:06:24 +0300

    remove promise

--- api/app/connectors/mongoose-connector.js
+++ api/app/connectors/mongoose-connector.js
@@ -1,20 +1,16 @@
 import mongoose from 'mongoose';
 
-mongoose.Promise = Promise;
-
 export default (mongoUri) => {
   if (!mongoUri) {
     throw Error('Mongo uri is undefined');
   }
-  return new Promise((res, rej) => {
-    mongoose
-        .connect(mongoUri, { useNewUrlParser: true })
-        .then((mongodb) => {
-          res(mongodb);
-          console.log('Mongo connected');
-        })
-        .catch((err) => {
-          rej(err);
-        });
-  });
+  mongoose
+      .connect(mongoUri, { useNewUrlParser: true })
+      .then((res) => {
+        console.log('Mongo connected');
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 };
